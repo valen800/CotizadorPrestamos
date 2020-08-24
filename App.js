9fd4eb4816f5fb97feda6e0e6d8ba97af321323a
valen800 <varulo.cvcv@gmail.com>
2020-08-24 09:02:01 +0200

    init Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  //SafeAreaView para centrar el texto
+  return (
+    <SafeAreaView>
+      <Text>Esto es la app principal</Text>
+    </SafeAreaView>
+  );
+}

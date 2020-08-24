ab218d952998a8e8c0ed1530b8b60bea6e53f1eb
valen800 <varulo.cvcv@gmail.com>
2020-08-24 15:01:28 +0200

    Formulario Cotización

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, StatusBar} from 'react-native';
+import Form from './src/components/Form'; //para una importacion default
+//import {FormV2} from './src/components/Form'; //para una importacion de un componente que no es default
+//import Form, {FormV2, ejemplo1} from './src/components/Form'; //para una importacion del componente default y todos los demas
+import colors from './src/utils/colors';
 
 export default function App() {
-  //SafeAreaView para centrar el texto
+  //SafeAreaView coloca los componentes dentro del perimetro de la pantalla
   return (
-    <SafeAreaView>
-      <Text>Esto es la app principal</Text>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.background} />
+        <Text style={styles.titleApp}>Cotizador de Prestamos</Text>
+        <Form />
+      </SafeAreaView>
+
+      <View>
+        <Text>Resultado</Text>
+      </View>
+
+      <View>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: 290, //px
+    alignItems: 'center',
+  },
+  titleApp: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginTop: 15,
+  },
+  background: {
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 200,
+    width: '100%',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    position: 'absolute',
+    zIndex: -1, //Pone por debajo de todos los elementos dentro del safeArea (solo funciona si tiene posicion relativa o absoluta)
+  },
+});
---
+++ src/components/Form.js
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+import {Picker} from '@react-native-community/picker';
+import RNPickerSelect from 'react-native-picker-select';
+import colors from '../utils/colors';
+
+export default function Form() {
+  const [PickerMonths, setPickerMonths] = useState('');
+
+  return (
+    <View style={styles.viewForm}>
+      <View style={styles.viewInputs}>
+        <TextInput
+          placeholder="Cantidad a pedir"
+          keyboardType="numeric"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Interes %"
+          keyboardType="numeric"
+          style={[styles.input, styles.inputPercentage]} //usar varios estilos envolver en un array todos los estilos
+        />
+      </View>
+      <View>
+        <RNPickerSelect
+          style={picketSelectStyles}
+          onValueChange={(value) => setPickerMonths(value)}
+          useNativeAndroidPickerStyle={false}
+          placeholder={{
+            label: 'Seleccióna los plazos...',
+            value: null,
+          }}
+          items={[
+            {label: '3 meses', value: 3},
+            {label: '6 meses', value: 6},
+            {label: '12 meses', value: 12},
+            {label: '24 meses', value: 24},
+          ]}
+        />
+      </View>
+    </View>
+  );
+}
+
+// export function FormV2() {
+//   return (
+//     <View>
+//       <TextInput placeholder="Cantidad a pedir" />
+//     </View>
+//   );
+// }
+
+const styles = StyleSheet.create({
+  viewForm: {
+    position: 'absolute', //para poder colocar dodne queramos los elementos
+    bottom: 0, //Si esta fuera del contenedor padres, es decir, tiene en los estilos -90 por ejemplo no funcionaran los componentes
+    width: '85%',
+    paddingHorizontal: 50,
+    backgroundColor: colors.PRIMARY_COLOR_DARK,
+    borderRadius: 30,
+    height: 180,
+    justifyContent: 'center',
+  },
+  viewInputs: {
+    flexDirection: 'row', //para poner inputs al lado del otro
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#fff',
+    borderColor: colors.PRIMARY_COLOR,
+    borderRadius: 5,
+    width: '60%',
+    marginLeft: -5,
+    marginRight: 5,
+    marginBottom: 10,
+    color: '#000',
+    paddingHorizontal: 20,
+  },
+  inputPercentage: {
+    width: '40%',
+    marginLeft: 5,
+  },
+});
+
+const picketSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 5,
+    color: 'black',
+    paddingRight: 30,
+    backgroundColor: '#fff',
+    marginLeft: -5,
+    marginRight: -5,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'grey',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30,
+    backgroundColor: '#fff',
+    marginLeft: -6,
+    marginRight: -6,
+  },
+});
